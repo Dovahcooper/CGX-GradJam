9e212907f053e11ff929e6019a1901d1e25a25ed
Matthew Holt <matthew.holt@uoit.net>
2020-05-03 01:21:29 -0400

    Boss Respawn
    boss respawns, writing spawn behaviours now

--- CGX_Jam/Assets/CGX ASSETS/Scripts/Boss/Boss.cs
+++ CGX_Jam/Assets/CGX ASSETS/Scripts/Boss/Boss.cs
@@ -1,43 +1,106 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class Boss : MonoBehaviour
 {
+    [SerializeField]
+    public UnityEvent spawnBehaviour;
+
     public Elements bossType;
 
-    public int health = 1000;
+    public int maxHealth = 750;
+    int health;
+
+    public int killCount = 0;
 
     public float dissolveTime = 0f;
 
     float dissolveSpeed = 0.2f;
+
+    public Text id;
+    public RawImage healthBar;
     
     [Range(-10f, 10f)]
     public float intensity = 4f;
 
     MeshRenderer rend;
 
-    bool dying;
+    public bool dying = false;
+
+    float startScaleBar;
 
     // Start is called before the first frame update
     void Start()
     {
+        health = maxHealth;
+
         rend = GetComponentInChildren<MeshRenderer>();
 
         float factor = Mathf.Pow(2f, intensity);
 
-        rend.material.SetColor("DissolveColour", Elemental.colours[(int)bossType] * factor);
+        Color newCol = Elemental.colours[(int)bossType];
+
+        rend.material.SetColor("DissolveColour", newCol * factor);
         rend.material.SetFloat("DissolveState", dissolveTime);
+
+        id.color = newCol;
+        healthBar.color = newCol;
+
+        startScaleBar = healthBar.rectTransform.localScale.x;
     }
 
-    private void OnEnable()
+    private void re_enable()
     {
-        dissolveTime = 0f;
+        killCount++;
+        if (killCount >= 4)
+        {
+        
+        }
+        else
+        {
+            int newType = (int)bossType;
 
-        float factor = Mathf.Pow(2, intensity);
+            spawnBehaviour.RemoveAllListeners();
 
-        rend.material.SetColor("DissolveColour", Elemental.colours[(int)bossType] * factor);
-        rend.material.SetFloat("DissolveState", dissolveTime);
+            bossType = (Elements)(++newType % 4);
+
+            dissolveSpeed /= 1.5f;
+            dissolveTime = 0f;
+            
+            health = maxHealth;
+            dying = false;
+
+            float factor = Mathf.Pow(2, intensity);
+
+            Color newCol = Elemental.colours[(int)bossType];
+
+            rend.material.SetColor("DissolveColour", newCol * factor);
+            rend.material.SetFloat("DissolveState", dissolveTime);
+
+            id.color = newCol;
+            healthBar.color = newCol;
+
+            switch (bossType)
+            {
+                case Elements.Fire:
+                    spawnBehaviour.AddListener(FlameSpawn);
+                    break;
+                case Elements.Water:
+                    spawnBehaviour.AddListener(WaterRise);
+                    break;
+                case Elements.Lightning:
+                    spawnBehaviour.AddListener(LightningStrikes);
+                    break;
+                case Elements.Wind:
+                    spawnBehaviour.AddListener(TornadoSpawn)
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -51,18 +114,31 @@ public class Boss : MonoBehaviour
             BeginDeath();
         }
 
-        if (dying)
+        if (dying && dissolveTime > 0f)
         {
             dissolveTime -= Time.fixedDeltaTime * dissolveSpeed;
         }
+        else if(dying && dissolveTime <= 0f)
+        {
+            re_enable();
+        }
+        else
+        {
+            //do nothing?
+        }
 
         rend.material.SetFloat("DissolveState", dissolveTime);
+
+        healthBar.transform.localScale = new Vector3(Mathf.Lerp(0f, startScaleBar, (float)health / maxHealth), healthBar.transform.localScale.y, 0f);
     }
 
     void BeginDeath()
     {
-        dying = true;
-        dissolveSpeed *= 2f;
+        if (!dying)
+        {
+            dying = true;
+            dissolveSpeed *= 1.5f;
+        }
     }
 
     public void Damage(Elements incoming)
@@ -150,5 +226,10 @@ public class Boss : MonoBehaviour
             //I just enjoy putting else statements
             //blame my high school teachers
         }
+    }
+
+    void FlameSpawn()
+    {
+
     }
 }
